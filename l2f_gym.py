54f7aaa1da991e74f7cb8dabb0e6d90ef7c9f3d1
korneelf1 <korneel.vandenberghe@hotmail.be>
2024-10-09 11:17:06 +0200

    using l2f obs and clean up the header

--- l2f_gym.py
+++ l2f_gym.py
@@ -79,7 +79,7 @@ def power_distribution_force_torque(control, arm_length=0.046, thrust_to_torque=
 # power_distribution_force_torque(control, motor_thrust_uncapped, arm_length=0.1, thrust_to_torque=0.05, pwm_to_thrust_a=0.01, pwm_to_thrust_b=0.02)
 
 class Learning2Fly(gym.Env):
-    def __init__(self, fast_learning=True,seed=None,rpm=False, action_history=True, quaternions_to_obs_matrices=True) -> None:
+    def __init__(self, fast_learning=True,seed=None) -> None:
 
         super().__init__()
         # L2F initialization
