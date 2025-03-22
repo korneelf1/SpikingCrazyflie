535d846fb72134eb186d4d0e5b6dfa3f86a323f6
korneel <korneel@harborai.net>
2025-03-21 23:54:59 -0300

    scheduling order rather then order

--- bash_scripts/td3bc_online_bash_cuda0.sh
+++ bash_scripts/td3bc_online_bash_cuda0.sh
@@ -1,5 +1,5 @@
-python TD3BC_Online.py --device='cuda' --hidden-sizes  256 128 --bc-factor 0.99 --bc-val 0.2 --jumpstart --curriculum --order 1
-python TD3BC_Online.py --device='cuda' --hidden-sizes  256 128 --bc-factor 0.99 --bc-val 0.2 --jumpstart --curriculum --order 0
-python TD3BC.py --device='cuda' --hidden-sizes  256 128 --bc-factor 0.99 --bc-val 0.2 --jumpstart --curriculum --order 1
-python TD3BC.py --device='cuda' --hidden-sizes  256 128 --bc-factor 0.99 --bc-val 0.2 --jumpstart --curriculum --order 1
+python TD3BC_Online.py --device='cuda' --hidden-sizes  256 128 --bc-factor 0.99 --bc-val 0.2 --jumpstart --curriculum --scheduling-order 1
+python TD3BC_Online.py --device='cuda' --hidden-sizes  256 128 --bc-factor 0.99 --bc-val 0.2 --jumpstart --curriculum --scheduling-order 0
+python TD3BC.py --device='cuda' --hidden-sizes  256 128 --bc-factor 0.99 --bc-val 0.2 --jumpstart --curriculum --scheduling-order 1
+python TD3BC.py --device='cuda' --hidden-sizes  256 128 --bc-factor 0.99 --bc-val 0.2 --jumpstart --curriculum --scheduling-order 1
 
