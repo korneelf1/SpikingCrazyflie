1fbe30a618697f79d99536660a327cd890cc9747
korneel <korneel@harborai.net>
2025-09-28 10:08:02 +0200

    save artifact correction

--- TD3BC_Online.py
+++ TD3BC_Online.py
@@ -518,8 +518,8 @@ class TD3BC_Online:
             n_epochs_tot+=epochs_per_gather
             cur_epoch+=epochs_per_gather
             filename = f"TD3BC_Online_TEMP_{self.timestamp}_epoch_{cur_epoch}.pth"
-            save_checkpoint(self.model.state_dict(), filename)
-            wandb.run.log_artifact(filename, name='policy_streaming', type='model')
+            checkpoint_path = save_checkpoint(self.model.state_dict(), filename)
+            wandb.run.log_artifact(checkpoint_path, name='policy_streaming', type='model')
             # Update curriculum every 6 epochs in the final training phase
             if self.curriculum and (cur_epoch // epochs_per_gather) % curriculum_interval == 0 and cur_epoch > 0:
                 self.env.update_curriculum()
